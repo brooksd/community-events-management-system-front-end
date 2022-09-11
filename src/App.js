418360f399cc01886c4fe9a5d349099e6e3e985d
Donald Brooks Arum <brooksd079@gmail.com>
2022-09-11 15:42:05 +0300

    updates the app.js with new routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Home from './components/Home';
+import Layout from "./components/layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddMeetUps from "./pages/AddMeetUps";
+import Favorites from "./pages/Favorites";
+import { MeetupsContextProvider } from "./store/meetups-context";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/signin'}>
-              Event Central
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/signup'}>
-                    Sign up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/signin'}>
-                    Login
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              
-              <Route path="/signin" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <MeetupsContextProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-meetup" element={<AddMeetUps />} />
+          <Route path="favorites" element={<Favorites />} />
+        </Routes>
+      </Layout>
+    </MeetupsContextProvider>
   );
-}
+};
 
 export default App;
